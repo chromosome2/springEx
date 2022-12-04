2a48a7dea1d0b7311f3ca4c773837f2c05a7df3a
chromosome2 <tnwls356@gmail.com>
2022-12-04 19:14:49 +0900

    221204

--- springMybatis/src/main/java/springMybatis/ex01/member/controller/MemberControllerImpl.java
+++ springMybatis/src/main/java/springMybatis/ex01/member/controller/MemberControllerImpl.java
@@ -1,4 +1,4 @@
-package com.spring.member.controller;
+package springMybatis.ex01.member.controller;
 
 import java.util.List;
 
@@ -8,28 +8,26 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-import com.spring.member.service.MemberService;
-import com.spring.member.vo.MemberVO;
+import springMybatis.ex01.member.service.MemberService;
+import springMybatis.ex01.member.vo.MemberVO;
 
 public class MemberControllerImpl extends MultiActionController implements MemberController{
+
 	private MemberService memberService;
 	
 	public void setMemberService(MemberService memberService) {
 		this.memberService=memberService;
 	}
-
-	//http://127.0.0.1:8090/springJDBC/member/listMembers.do
+	
 	@Override
 	public ModelAndView listMembers(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String viewName=getViewName(request);
 		List memberList=memberService.listMembers();
-		ModelAndView mav=new ModelAndView(viewName);//응답할 view로 바로 값 저장
+		ModelAndView mav=new ModelAndView(viewName);
 		mav.addObject("memberList",memberList);
 		return mav;
 	}
-
-	//http://127.0.0.1:8090/springJDBC/member/memberForm.do
-	@Override
+	
 	public ModelAndView memberForm(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String viewName=getViewName(request);
 		ModelAndView mav=new ModelAndView(viewName);
@@ -40,19 +38,41 @@ public class MemberControllerImpl extends MultiActionController implements Membe
 	public ModelAndView addMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		request.setCharacterEncoding("utf-8");
 		MemberVO memberVO=new MemberVO();
-		String id=request.getParameter("id");
-		String pwd=request.getParameter("pwd");
-		String name=request.getParameter("name");
-		String email=request.getParameter("email");
-		memberVO.setId(id);
-		memberVO.setPwd(pwd);
-		memberVO.setName(name);
-		memberVO.setEmail(email);
+		bind(request,memberVO);//원래 내가 했던 request.getParameter 와 memberVO.set 했떤 작업. 얘가 자동으로 해줌. Springframework에서 제공하는 Multiactioncontroller에서 제공해주는 bind임.
+		//vo의 칼럼과 받아오는 name이 같아야 가능.
 		int result=memberService.addMember(memberVO);
-		List memberList=memberService.listMembers();
-		ModelAndView mav=new ModelAndView("listMembers");
-		//redirect를 안쓰려면 이렇게 해야함.
-		mav.addObject("memberList",memberList);
+		ModelAndView mav=new ModelAndView("redirect:/member/listMembers.do");
+		//redirect를 하는 이유는 우리가 필요한건 listMembers.jsp가 아니기 때문. listMembers.do이기 떄문임.
+		return mav;
+	}
+
+	@Override
+	public ModelAndView removeMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		String id=request.getParameter("id");
+		memberService.removeMember(id);
+		ModelAndView mav=new ModelAndView("redirect:/member/listMembers.do");
+		return mav;
+	}
+
+	@Override
+	public ModelAndView modMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		String viewName=getViewName(request);
+		String id=request.getParameter("id");
+		MemberVO memberVO=new MemberVO();
+		memberVO=memberService.findMember(id);
+		ModelAndView mav=new ModelAndView(viewName);
+		mav.addObject("member",memberVO);
+		return mav;
+	}
+
+	@Override
+	public ModelAndView updateMember(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		request.setCharacterEncoding("utf-8");
+		MemberVO memberVO=new MemberVO();
+		bind(request, memberVO);
+		int result=memberService.updateMember(memberVO);
+		ModelAndView mav=new ModelAndView("redirect:/member/listMembers.do");
 		return mav;
 	}
 	
@@ -85,6 +105,5 @@ public class MemberControllerImpl extends MultiActionController implements Membe
 		}
 		return fileName;
 	}
-	
-	
+
 }
